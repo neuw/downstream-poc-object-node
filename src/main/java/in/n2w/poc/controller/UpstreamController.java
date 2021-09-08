6245e7bd916dbda1cb92518f257d1217a2443806
Karanbir Singh <krnbr@live.in>
2021-09-08 20:33:56 +0530

    example of the request body as ObjectNode as well

--- src/main/java/in/n2w/poc/controller/UpstreamController.java
+++ src/main/java/in/n2w/poc/controller/UpstreamController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -29,6 +31,11 @@ public class UpstreamController {
     @GetMapping("/v1/mock/upstream")
     public ObjectNode getResponseViaUpstream() {
         return restTemplate.getForObject(URI.create(DOWNSTREAM_URL), ObjectNode.class);
+    }
+
+    @PostMapping("/v1/mock/upstream")
+    public ObjectNode postObject(@RequestBody final ObjectNode requestBody) {
+        return requestBody;
     }
 
     // wrong approach
